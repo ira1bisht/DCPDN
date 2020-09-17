b9d9ef65617a57d0754191454c642ba1ec0a9790
ira1bisht <ira1bisht@gmail.com>
2020-09-17 18:16:55 +0530

    Update create_train.py

--- create_train.py
+++ create_train.py
@@ -80,6 +80,13 @@ img_size = 224
 # pdb.set_trace()
 total_num = 0
 plt.ion()
+
+coloured=np.array([[  0.8,         0.8,         0.8       ],
+                   [161.93557207,  96.6134324,   30.37198881],
+                   [219.21343269, 164.85414804, 187.38114257],
+                   [127.85954953, 145.75792237, 173.37709691],
+                   [125.54341886, 125.54341886, 125.54341886]])
+beeta=np.array([1,3,5])
 for index in range(1000):
     index = index
     gt_image = (image[index, :, :, :]).astype(float)
@@ -104,31 +111,32 @@ for index in range(1000):
     if gt_depth.shape != (img_size, img_size):
         continue
 
-    for j in range(8):
+    for j in range(5):
+        
+        for k in range(3):
 
-        beta = uniform(0.5, 2)
+            beta = beeta[k]
 
-        tx1 = np.exp(-beta * gt_depth)
+            tx1 = np.exp(-beta * gt_depth)
 
-        a = 1 - 0.5 * uniform(0, 1)
+            A=coloured[j];
 
 
-        m = gt_image.shape[0]
-        n = gt_image.shape[1]
+            m = gt_image.shape[0]
+            n = gt_image.shape[1]
 
-        rep_atmosphere = np.tile(np.reshape(A, [1, 1, 3]), [m, n, 1])
-        tx1 = np.reshape(tx1, [m, n, 1])
+            rep_atmosphere = np.tile(np.reshape(A, [1, 1, 3]), [m, n, 1])
+            tx1 = np.reshape(tx1, [m, n, 1])
 
-        max_transmission = np.tile(tx1, [1, 1, 3])
+            max_transmission = np.tile(tx1, [1, 1, 3])
 
-        haze_image = gt_image * max_transmission + rep_atmosphere * (1 - max_transmission)
+            haze_image = gt_image * max_transmission + rep_atmosphere * (1 - max_transmission)
 
-        total_num = total_num + 1
-        scipy.misc.imsave('a0.9beta1.29.jpg', haze_image)
-        scipy.misc.imsave('gt.jpg', gt_image)
+            total_num = total_num + 1
+            scipy.misc.imsave('a0.9beta1.29.jpg', haze_image)
+            scipy.misc.imsave('gt.jpg', gt_image)
 
-        h5f=h5py.File('./facades/train/'+str(total_num)+'.h5','w')
-        h5f.create_dataset('haze',data=haze_image)
-        h5f.create_dataset('trans',data=max_transmission)
-        h5f.create_dataset('atom',data=rep_atmosphere)
-        h5f.create_dataset('gt',data=gt_image)
+            h5f=h5py.File('./facades/train/'+str(i)+'_'+str(j)+'_'+str(k)+'.h5','w')
+            h5f.create_dataset('haze',data=haze_image)
+            h5f.create_dataset('trans',data=max_transmission)
+            h5f.create_dataset('gt',data=gt_image)
